cdfbe0a7acb094d231a5156b8786481daec1a92d
Ivan Shvets <37488015+Leocete@users.noreply.github.com>
2019-02-11 23:14:56 +0200

    Removed ES6 syntax

--- FLX_homework_8/homework/js/formatTime.js
+++ FLX_homework_8/homework/js/formatTime.js
@@ -4,7 +4,7 @@ function formatTime(min) {
     let numhours = Math.floor((min % 1440) / 60);
     let numminutes = min % 60;
 
-    return `${numdays} day(s) ${numhours} hour(s) ${numminutes} minute(s)`;
+    return numdays + ' day(s) ' + numhours + ' hour(s) ' + numminutes + ' minute(s).';
 
 }
 console.log(formatTime(59));
